223a3fb0a8b5b9b9ccd4eddd03baf7f91885c272
Pranav Ajay <pranavajay2002@gmail.com>
2022-04-20 23:39:59 +0530

    Updated os name animation

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-import os from 'os';
-console.log(os.version());
+import os, { hostname } from 'os';
+import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+
+const osName = os.hostname().toUpperCase();
+
+const sleep = (ms = 5000) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function dOs(){
+    const rainbowTitle = chalkAnimation.rainbow(figlet.textSync(osName ));
+    await sleep();
+    rainbowTitle.stop();
+  };
+    
+await dOs();
